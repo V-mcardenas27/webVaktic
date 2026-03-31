9d513a862c064dbd287807bf0524e971f84afe58
V-mcardenas27 <mcardenas27.v@gmail.com>
2026-03-31 14:05:16 -0500

    analitycs vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,9 @@ import vercel from '@astrojs/vercel/serverless';
 export default defineConfig({
     output: 'server',
     // @ts-ignore
-    adapter: vercel(),
+    adapter: vercel({
+        webAnalytics: {
+            enabled: true, // set to false when using @vercel/analytics@1.4.0
+        },
+    }),
 });
